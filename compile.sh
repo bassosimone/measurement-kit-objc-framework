9dff475448f88c06d21a66676f9d00ada8dcd277
Simone Basso <bassosimone@gmail.com>
2015-06-17 23:51:28 +0200

    compile.sh not needed anymore

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-# XXX: Currently this works on my system only
-
-clang++ -std=c++11 -fobjc-arc -Wall -I $HOME/src/measurement-kit/measurement-kit-app-ios/Libight_iOS/Libight_iOS/include/ -c MeasurementKit/MeasurementKit.mm
-
-clang++ -fobjc-arc -Wall -c main.m
-
-clang++ -fobjc-arc -o main main.o MeasurementKit.o $HOME/src/measurement-kit/measurement-kit-app-ios/Libight_iOS/Libight_iOS/lib/*.a -framework Foundation
