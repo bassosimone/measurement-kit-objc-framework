6417add94f0708356b25116d4f120e8e8c775133
Simone Basso <bassosimone@gmail.com>
2015-06-17 20:45:27 +0200

    Simplify compile.sh

--- compile.sh
+++ compile.sh
@@ -2,9 +2,8 @@
 
 # XXX: Currently this works on my system only
 
-clang++ -std=c++11 -fobjc-arc -Wall -I $HOME/src/measurement-kit/measurement-kit-app-ios/Libight_iOS/Libight_iOS/include/ -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/Foundation.framework/ -c MeasurementKit/MeasurementKit.mm
+clang++ -std=c++11 -fobjc-arc -Wall -I $HOME/src/measurement-kit/measurement-kit-app-ios/Libight_iOS/Libight_iOS/include/ -c MeasurementKit/MeasurementKit.mm
 
 clang++ -fobjc-arc -Wall -c main.m
 
-clang++ -fobjc-arc -o main main.o MeasurementKit.o $HOME/src/measurement-kit/measurement-kit-app-ios/Libight_iOS/Libight_iOS/lib/*.a -L /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/System/Library/Frameworks/Foundation.framework/ -framework Foundation
-
+clang++ -fobjc-arc -o main main.o MeasurementKit.o $HOME/src/measurement-kit/measurement-kit-app-ios/Libight_iOS/Libight_iOS/lib/*.a -framework Foundation
